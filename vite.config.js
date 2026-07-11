1983d39d57ac028eb66a0da3266dbe1f6f8d8ddf
Timilehin Olajolo <timilehinolajolo@gmail.com>
2026-07-11 18:54:33 +0100

    Update base path to /my-portfolio/

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // THIS MUST MATCH YOUR REPOSITORY NAME
+  base: '/my-portfolio/', 
 })
